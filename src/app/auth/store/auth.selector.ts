9eae947b95811a2fa9fab525df831ded740fa747
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2026-01-11 06:02:56 +0200

    [update] use feature selector instead of create feature selector

--- src/app/auth/store/auth.selector.ts
+++ src/app/auth/store/auth.selector.ts
@@ -1,10 +1,11 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { User } from "../model/user.model";
+import { authFeature } from "./auth.feature";
 
 const authState = createFeatureSelector<User>("auth");
 
 export const isLoggedInSelector = createSelector(
-  authState,
+  authFeature.selectAuthState,
   (state) => !!state.email
 );
 // !isLoggedInSelector => can't negate selector instead make selector map value then continue the logic in another map function
