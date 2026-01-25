c24c5381bfd93b0fb7cd494f1ce77b52029cc02f
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2026-01-25 11:55:43 +0200

    [add] ngrx-store-localstorage reducer to persist state

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,4 +1,5 @@
 import { ActionReducer, MetaReducer } from "@ngrx/store";
+import { localStorageSync } from "ngrx-store-localstorage";
 
 function loggerMetaReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   return (state, action) => {
@@ -8,19 +9,30 @@ function loggerMetaReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   };
 }
 
-function syncStateToLS(reducer: ActionReducer<any>): ActionReducer<any> {
-  return (state, action) => {
-    const nextReducer = reducer(state, action);
-    if (action.type !== "@ngrx/store/init") {
-      localStorage.setItem("appState", JSON.stringify(nextReducer));
-    }
-    return nextReducer;
-  };
+// function syncStateToLS(reducer: ActionReducer<any>): ActionReducer<any> {
+//   return (state, action) => {
+//     const nextReducer = reducer(state, action);
+//     if (action.type !== "@ngrx/store/init") {
+//       localStorage.setItem("appState", JSON.stringify(nextReducer));
+//     }
+//     return nextReducer;
+//   };
+// }
+
+function ngrxLsStore(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys: [
+      "auth", // define state to be persist or part of the state
+      // { user: ["profile", "preferences"] }, // only persist specific properties
+    ],
+    rehydrate: true, // refetch data from LS and set at state after page reload
+    storage: localStorage, // define place to store on it can be (LS , SS , IDB)
+  })(reducer);
 }
 
 export const appMetaReducers: MetaReducer<any>[] = [
   loggerMetaReducer,
-  syncStateToLS,
+  ngrxLsStore,
 ];
 
 export function getInitialAppState() {
