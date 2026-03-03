c5fc6900e65585c4c83c2d4a02cd8409ac53e86c
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2026-03-04 01:07:50 +0200

    refactor: make resolver based on course loaded state

--- src/app/courses/courses.resolver.ts
+++ src/app/courses/courses.resolver.ts
@@ -5,20 +5,25 @@ import {
   RouterStateSnapshot,
 } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { coursesSelector } from "./store/course.selector";
-import { tap } from "rxjs/operators";
+import { coursesLoadedSelector } from "./store/course.selector";
+import { filter, finalize, first, tap } from "rxjs/operators";
 import { coursesActions } from "./store/course.actions";
+import { CoursesEntity } from "./store/course.reducer";
 
 export const coursesResolver: ResolveFn<any> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot,
 ) => {
-  const _store = inject(Store);
-  return _store.select(coursesSelector).pipe(
+  const _store = inject(Store<CoursesEntity>);
+  let loading = false;
+  return _store.select(coursesLoadedSelector).pipe(
     tap((data) => {
-      if (!data.length) {
+      if (!data && !loading) {
         _store.dispatch(coursesActions.loadAllCourses());
       }
     }),
+    filter((data) => !data),
+    first(),
+    finalize(() => (loading = false)),
   );
 };
