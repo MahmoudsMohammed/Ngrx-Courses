75a2e5b6873fe3fd8ebba845f3d7cf0be26efe13
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2026-04-09 11:43:02 +0200

    refactor: use resolver to fetch course entity data based on loaded or not

--- src/app/courses/courses.resolver.ts
+++ src/app/courses/courses.resolver.ts
@@ -1,10 +1,16 @@
 import { inject } from "@angular/core";
 import { ResolveFn } from "@angular/router";
 import { CourseEntityService } from "./store/course.entity.service";
-import { tap } from "rxjs/operators";
+import { first, tap } from "rxjs/operators";
 
 export const courseResolverFn: ResolveFn<boolean> = (route, state) => {
   const _courseEntityService = inject(CourseEntityService);
-  _courseEntityService.getAll();
-  return _courseEntityService.loaded$.pipe(tap((state) => console.log(state)));
+  return _courseEntityService.loaded$.pipe(
+    tap((loaded) => {
+      if (!loaded) {
+        _courseEntityService.getAll();
+      }
+    }),
+    first(),
+  );
 };
